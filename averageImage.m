5a4f90bf037b5e24109b3678570e24f325471597
Jan Brejcha <brejchaja@gmail.com>
2017-05-19 17:38:23 +0200

    Implemented calculation of average image in the dataset.

---
+++ averageImage.m
@@ -0,0 +1,21 @@
+function [avgimg] = averageImage(dbTrain, batchsize)
+    if (nargin < 2)
+        batchsize = 100;
+    end
+    
+    imageFns= [ strcat( dbTrain.qPath, dbTrain.qImageFns(:)); ...
+        strcat( dbTrain.dbPath, dbTrain.dbImageFns(:) ) ];
+    
+    avgimg = vl_imreadjpeg(imageFns(1)); avgimg = avgimg{1};
+    avgimg = zeros(size(avgimg));
+    numImgs = size(imageFns, 1);
+    if numImgs < 2
+        return
+    end
+    
+    for i = 1 : batchsize : numImgs - batchsize
+        ims_ = vl_imreadjpeg(imageFns(i:i+batchsize)); 
+        ims = cat(4, ims_{:});
+        avgimg = avgimg * ((i-batchsize)/i) + sum(ims, 4) / i;
+    end
+end
