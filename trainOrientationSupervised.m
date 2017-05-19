aed4412b8c22a23344417fbd350cb450f000765c
Jan Brejcha <brejchaja@gmail.com>
2017-05-19 11:06:20 +0200

    Implemented classical triplet loss, improved training.

--- trainOrientationSupervised.m
+++ trainOrientationSupervised.m
@@ -8,7 +8,7 @@ function [ output_args ] = trainOrientationSupervised( dbTrain, dbVal, varargin 
         'method', 'vlad_preL2_intra', ...
         'batchSize', 4, ...
         'learningRate', 0.0001, ...
-        'lrDownFreq', 5, ...
+        'lrDownFreq', 30, ...
         'lrDownFactor', 2, ...
         'weightDecay', 0.001, ...
         'momentum', 0.9, ...
@@ -17,7 +17,7 @@ function [ output_args ] = trainOrientationSupervised( dbTrain, dbVal, varargin 
         'nNegChoice', 10, ...
         'nNegCap', 10, ...
         'nNegCache', 10, ...
-        'nEpoch', 30, ...
+        'nEpoch', 10, ...
         'margin', 0.1, ...
         'excludeVeryHard', false, ...
         'jitterFlip', false, ...
@@ -47,7 +47,8 @@ function [ output_args ] = trainOrientationSupervised( dbTrain, dbVal, varargin 
         );
     paths = localPaths();
     opts = vl_argparse(opts, varargin);
-    
+    [obj, auxData] = initObj(dbTrain);
+        
     if isempty(opts.sessionID),
         if opts.startEpoch>1, error('Have to specify sessionID to restart'); end
         rng('shuffle'); opts.sessionID= relja_randomHex(4);
@@ -87,131 +88,202 @@ function [ output_args ] = trainOrientationSupervised( dbTrain, dbVal, varargin 
     
     losses = [];
     lr = opts.learningRate;
-    
-    for iBatch = 1 : nBatches
-        qIDs = trainOrder((iBatch-1)*opts.batchSize + (1:opts.batchSize));
-        allRes = [];
-        thisBatchSize = 0;
-        for iQuery = 1 : opts.batchSize
-            wait(gpuDevice());
-            %% get positive, and negative samples for current query
-            qID = qIDs(iQuery);
-            posIDs = dbTrain.nontrivialPosQ(qID);
-            if isempty(posIDs), continue; end
-            negIDs = dbTrain.sampleNegsQ(qID, opts.nNegChoice)
-            labels = [0 ones(1, size(posIDs, 1)) -ones(1, size(negIDs, 1))];
-            thisBatchSize = thisBatchSize + size(labels, 2) - 1; %TODO check if correct
-            %% load images
-            [ims, thisNumIms] = loadImages(qID, posIDs, negIDs, dbTrain, net, opts);
+    figbatchloss = figure('Name', 'Loss per batch');
+    for iEpoch = 1 : opts.nEpoch
+        ID = sprintf('ep%06d_latest', iEpoch);
+        trainID = sprintf('%s_train', ID);
+        valID = sprintf('%s_val', ID);
+        for iBatch = 1 : nBatches
+            qIDs = trainOrder((iBatch-1)*opts.batchSize + (1:opts.batchSize));
+            allRes = [];
+            thisBatchSize = 0;
+            for iQuery = 1 : opts.batchSize
+                %% get positive, and negative samples for current query
+                qID = qIDs(iQuery);
+                posIDs = dbTrain.nontrivialPosQ(qID);
+                if isempty(posIDs), continue; end
+                negIDs = dbTrain.sampleNegsQ(qID, opts.nNegChoice);
+                labels = [0 ones(1, size(posIDs, 1)) -ones(1, size(negIDs, 1))];
+                thisBatchSize = thisBatchSize + size(labels, 2) - 1; %TODO check if correct
+                %% load images
+                [ims, thisNumIms] = loadImages(qID, posIDs, negIDs, dbTrain, net, opts);
 
-            %% forward pass
-            % the memory saving related to backPropDepth is obayed 
-            %implicitly due to running netPrepareForTrain before, see the 
-            %comments in the function for an explanation
-            gpuDevice()
-            res = vl_simplenn(net, ims, [], [], 'mode', ...
-                'normal', 'conserveMemory', true); 
-            
-            % because of the 'conserveMemory' the input is deleted, 
-            % restore it if needed
-            if opts.backPropToLayer==1, res(1).x= ims; end 
-            ims = [];
-            feats = reshape( gather(res(end).x), [], thisNumIms );
+                %% forward pass
+                % the memory saving related to backPropDepth is obayed 
+                %implicitly due to running netPrepareForTrain before, see the 
+                %comments in the function for an explanation
+                res = vl_simplenn(net, ims, [], [], 'mode', ...
+                    'normal', 'conserveMemory', true); 
 
-            %% loss
-            x_q = feats(:, find(labels == 0));   %query feats
-            x_p = feats(:, find(labels == 1));   %positive feats
-            x_n = feats(:, find(labels == -1));  %negative feats
-            
-            neg_denom = sum(sum((repmat(x_q, [1 size(x_n, 2)]) - x_n).^2));
-            E_p = single(sum(sum((repmat(x_q, [1 size(x_p, 2)]) - x_p).^2)));
-            E_n = single(1.0./neg_denom);
-            E = E_p + E_n;
-            losses(end + 1) = E;
-            %% backpropagation
-            neg_denom_sq = neg_denom.^2;
-            dEdx_q = 2.*sum(repmat(x_q, [1 size(x_p, 2)]) - x_p, 2) + ...
-                2.*sum(repmat(x_q, [1 size(x_n, 2)]) - x_n, 2) ./ neg_denom_sq;
-            
-            dEdx_p = -2 .* repmat(x_q, [1 size(x_p, 2)]) - x_p;
-            dEdx_n = -2 .* repmat(x_q, [1 size(x_n, 2)]) - x_n ./ neg_denom_sq;
-            
-            dEdx = [dEdx_q dEdx_p dEdx_n];
+                % because of the 'conserveMemory' the input is deleted, 
+                % restore it if needed
+                if opts.backPropToLayer==1, res(1).x= ims; end 
+                ims = [];
+                feats = reshape( gather(res(end).x), [], thisNumIms );
 
-            if opts.useGPU
-                dEdx = gpuArray(dEdx);
-            end
-            allRes = [allRes ; vl_simplenn(net, ims, dEdx, res, ...
-                        'mode', 'normal', ...
-                        'skipForward', true, ...
-                        'backPropDepth', opts.backPropDepth, ...
-                        'conserveMemory', true)];
-            clear dEdx_q;
-            clear dEdx_p;
-            clear dEdx_n;
-            clear E_p;
-            clear E_n; 
-            clear E;
-            clear x_p;
-            clear x_q;
-            clear x_n;
-            clear negDenom;
-            clear labels;
-            clear res;
-            clear feats;
-            clear dEdx;
+                %% my loss
+                
+                x_q = feats(:, find(labels == 0));   %query feats
+                x_p = feats(:, find(labels == 1));   %positive feats
+                x_n = feats(:, find(labels == -1));  %negative feats
+
+                neg_denom = sum(sum((repmat(x_q, [1 size(x_n, 2)]) - x_n).^2));
+                E_p = single(sum(sum((repmat(x_q, [1 size(x_p, 2)]) - x_p).^2)));
+                E_n = single(1.0./neg_denom);
+                E = E_p + E_n;
+                losses(end + 1) = E;
+                %% backpropagation
+                neg_denom_sq = neg_denom.^2;
+                dEdx_q = 2.*sum(repmat(x_q, [1 size(x_p, 2)]) - x_p, 2) + ...
+                    2.*sum(repmat(x_q, [1 size(x_n, 2)]) - x_n, 2) ./ neg_denom_sq;
+
+                dEdx_p = -2 .* repmat(x_q, [1 size(x_p, 2)]) - x_p;
+                dEdx_n = -2 .* repmat(x_q, [1 size(x_n, 2)]) - x_n ./ neg_denom_sq;
+
+                dEdx = [dEdx_q dEdx_p dEdx_n];
+
+                
+                
+                %% triplet loss
+                %{
+                x_q = feats(:, find(labels == 0));   %query feats
+                x_p = feats(:, find(labels == 1));   %positive feats
+                x_n = feats(:, find(labels == -1));  %negative feats
+
+                E_p = single(sum((repmat(x_q, [1 size(x_p, 2)]) - x_p).^2, 2));
+                E_n = single(sum((repmat(x_q, [1 size(x_n, 2)]) - x_n).^2, 2));
            
-        end %iQuery
-        
-        %% update net
-        %updateNet(net, opts, lr, thisBatchSize, allRes);
-        if size(allRes, 1) > 0
-            for l= 1:numel(net.layers)
-                for j= 1:numel(allRes(1, l).dzdw)
-                    if ismember(net.layers{l}.name, opts.fixLayers) continue; end
+                E_term = E_p - E_n;
+                E = sum(max(E_term, 0));
+                
+                losses(end + 1) = E;
+                
+                %% triplet loss backpropagation
+                dEdx_q = sum(2 .* (repmat(x_q, [1 size(x_p, 2)]) - x_p), 2);
+                dEdx_p = -2 .* (repmat(x_q, [1 size(x_p, 2)]) - x_p);
+                dEdx_n = 2 .* (repmat(x_q, [1 size(x_n, 2)]) - x_n);
+                
+                dEdx_q(find(E_term < 0), :) = 0;
+                dEdx_p(find(E_term < 0), :) = 0;
+                dEdx_n(find(E_term < 0), :) = 0;
 
-                    dzdw= allRes(1, l).dzdw{j};
-                    for iQuery= 2:size(allRes,1)
-                        dzdw= dzdw + allRes(iQuery, l).dzdw{j};
-                    end
-
-                    thisDecay= opts.weightDecay * net.layers{l}.weightDecay(j);
-                    thisLR= lr * net.layers{l}.learningRate(j);
-
-                    net.layers{l}.momentum{j}= ...
-                        opts.momentum * net.layers{l}.momentum{j} ...
-                        - thisDecay * net.layers{l}.weights{j} ...
-                        - (1 / thisBatchSize) * dzdw;
-                    net.layers{l}.weights{j}= net.layers{l}.weights{j} + thisLR * net.layers{l}.momentum{j};
-                    clear dzdw;
+                dEdx = [dEdx_q dEdx_p dEdx_n];
+                %}
+                if opts.useGPU
+                    dEdx = gpuArray(dEdx);
                 end
-            end
+                allRes = [allRes ; vl_simplenn(net, ims, dEdx, res, ...
+                            'mode', 'normal', ...
+                            'skipForward', true, ...
+                            'backPropDepth', opts.backPropDepth, ...
+                            'conserveMemory', true)];
+                clear dEdx_q;
+                clear dEdx_p;
+                clear dEdx_n;
+                clear E_p;
+                clear E_n; 
+                clear E;
+                clear x_p;
+                clear x_q;
+                clear x_n;
+                clear negDenom;
+                clear labels;
+                clear res;
+                clear feats;
+                clear dEdx;
+
+            end %iQuery
+
+            %% update net
+            updateNet(net, opts, lr, thisBatchSize, allRes);
+            plotLoss(losses, figbatchloss);
+            %testNet(dbTrain, dbVal, net, opts, obj, auxData, iEpoch,...
+            %        trainID, valID); %just to try whether this works
+            relja_display('Batch: %f, loss: %f.', iBatch, losses(end));
         end
-        clear allRes;
+        %% update lr
+        if mod(iEpoch, opts.lrDownFreq) == 0
+            newlr = lr / opts.lrDownFactor;
+            relja_display('Changing Learning rate from: %f to %f.', lr, newlr);
+            lr = newlr;
+            clear newlr;
+        end
         
-        %{
-        if opts.useGPU    
-            net = relja_simplenn_move(net, 'cpu');
-            save net.mat net losses trainOrder opts dbTrain dbVal nBatches
-            clear all;
-            gpu = gpuDevice(1);
-            load('net.mat', 'net', 'losses', 'trainOrder', 'opts', 'dbTrain', 'dbVal', 'nBatches');
-            net = relja_simplenn_move(net, 'gpu');
-            wait(gpu);
+        %% test
+        testNow = iEpoch==opts.nEpoch || rem(iEpoch, opts.epochTestFrequency) == 0;
+        if testNow
+            testNet(dbTrain, dbVal, net, opts, obj, auxData, iEpoch,...
+                    trainID, valID);
         end
-        %}
-    
-        plotAll(losses);
+        relja_display('Epoch: %f.', iEpoch);
+        %save net.mat net
     end
 end
 
-function plotAll(losses)
-    plot(losses);
+function [obj, auxData] = initObj(dbTrain)
+    obj= struct();
+    obj.train= struct('loss', [], 'recall', [], 'rankloss', []);
+    obj.val= struct('loss', [], 'recall', [], 'rankloss', []);
+    
+    auxData= {};
+    auxData.epochStartTime= {};
+    auxData.numTrain= dbTrain.numQueries;
+end
+
+function testNet(dbTrain, dbVal, net, opts, obj, auxData, iEpoch,...
+                 trainID, valID)
+    
+    [qFeatVal, dbFeatVal] = computeAllFeats(dbVal, net, opts, valID, true);
+    [obj.val.recall(:, end+1), obj.val.rankloss(:, end+1) ...
+        ] = testNet(dbVal, net, opts, valID, qFeatVal, dbFeatVal);
+    clear qFeatVal dbFeatVal;
+
+
+    [qFeat, dbFeat]= computeAllFeats(dbTrain, net, opts, trainID, true);
+
+
+    [obj.train.recall(:, end+1), obj.train.rankloss(:, end+1) ...
+        ]= testNet(dbTrain, net, opts, trainID, qFeat, dbFeat);
+
+    % to save the results
+    saveNet(net, obj, opts, auxData, ID, sprintf('epoch-end %d', iEpoch));
+
+    if opts.doDraw, plotResults(obj, opts, auxData); end
+
+end
+
+function plotLoss(losses, figloss)
+    figure(figloss);
+    semilogy(losses);
     drawnow update
 end
 
 function [net] = updateNet(net, opts, lr, thisBatchSize, allRes)
-    
+    if size(allRes, 1) > 0
+        for l= 1:numel(net.layers)
+            for j= 1:numel(allRes(1, l).dzdw)
+                if ismember(net.layers{l}.name, opts.fixLayers) 
+                    continue; 
+                end
+
+                dzdw= allRes(1, l).dzdw{j};
+                for iQuery= 2:size(allRes,1)
+                    dzdw= dzdw + allRes(iQuery, l).dzdw{j};
+                end
+
+                thisDecay= opts.weightDecay * net.layers{l}.weightDecay(j);
+                thisLR= lr * net.layers{l}.learningRate(j);
+
+                net.layers{l}.momentum{j}= ...
+                    opts.momentum * net.layers{l}.momentum{j} ...
+                    - thisDecay * net.layers{l}.weights{j} ...
+                    - (1 / thisBatchSize) * dzdw;
+                net.layers{l}.weights{j}= net.layers{l}.weights{j} + thisLR * net.layers{l}.momentum{j};
+                clear dzdw;
+            end
+        end
+    end
+    clear allRes;
 end
 
 function [ims, num] = loadImages(qID, posIDs, negIDs, dbTrain, net, opts)
@@ -230,4 +302,30 @@ function [ims, num] = loadImages(qID, posIDs, negIDs, dbTrain, net, opts)
         ims= gpuArray(ims);
     end
 end
+
+
+function [qFeat, dbFeat]= computeAllFeats(db, net, opts, ID, delFile)
+    if nargin<5, delFile= true; end
+    outPrefix= sprintf('%s%s_%s', opts.outPrefix, opts.sessionID, ID);
+
+    qFeatFn= sprintf('%s_q.bin', outPrefix);
+    tmpFn= sprintf('%s.tmp', qFeatFn);
+    serialAllFeats(net, db.qPath, db.qImageFns, ...
+        tmpFn, 'useGPU', opts.useGPU, 'batchSize', opts.computeBatchSize);
+    movefile(tmpFn, qFeatFn);
+
+    qFeat= fread( fopen(qFeatFn, 'rb'), inf, 'float32=>single');
+    qFeat= reshape(qFeat, [], db.numQueries);
+    if delFile, delete(qFeatFn); end
+
+    dbFeatFn= sprintf('%s_db.bin', outPrefix);
+    tmpFn= sprintf('%s.tmp', dbFeatFn);
+    serialAllFeats(net, db.dbPath, db.dbImageFns, ...
+        tmpFn, 'useGPU', opts.useGPU, 'batchSize', opts.computeBatchSize);
+    movefile(tmpFn, dbFeatFn);
+
+    dbFeat= fread( fopen(dbFeatFn, 'rb'), [size(qFeat,1), db.numImages], 'float32=>single');
+    if delFile, delete(dbFeatFn); end
+end
+
 
